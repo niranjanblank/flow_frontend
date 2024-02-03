a57410dd50e2413d9b85525385c582cc62ceb8e9
Niranjan Shah <niranjanshah474@gmail.com>
2024-02-03 23:25:49 +1100

    Popover added that will be used to render the create board form

---
+++ app/components/Dialogs/Popover.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React, { useState, useRef, useEffect } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+  content: React.ReactNode;
+}
+
+const Popover = ({ children, content }: Props) => {
+  const [show, setShow] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (popoverRef.current && !popoverRef.current.contains(event.target as Node)) {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside as unknown as (e: MouseEvent) => void);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside as unknown as (e: MouseEvent) => void);
+    };
+  }, []);
+
+  return (
+    <div ref={popoverRef} className="relative">
+      <div onClick={() => setShow(!show)}>
+        {children}
+      </div>
+      {show && (
+        <div className="absolute z-10 border text-black border-gray-300 bg-white p-2 rounded-lg shadow-md">
+          {content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Popover;
--- app/components/Navbar/Sidebar.tsx
+++ app/components/Navbar/Sidebar.tsx
@@ -1,18 +1,22 @@
 import { Navbar } from "./Navbar";
 import { FaPlus } from "react-icons/fa"
+import Popover from "../Dialogs/Popover";
 
 export default function Sidebar(){
     return (
         <Navbar>
-            <div className="flex flex-col  gap-1">
-                <div className="flex justify-between text-sm">
-                    <h1>Your boards</h1>
-                   <button><FaPlus/></button></div>
-                <ul className="text-xs ml-1 flex flex-col gap-1">
-                    <li>Board 1</li>
-                    <li>Board 2</li>
-                </ul>
-            </div>
-        </Navbar>
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between text-sm">
+            <h1>Your boards</h1>
+            <Popover content={<p>Add new board</p>}>
+              <button><FaPlus /></button>
+            </Popover>
+          </div>
+          <ul className="text-xs ml-1 flex flex-col gap-1">
+            <li>Board 1</li>
+            <li>Board 2</li>
+          </ul>
+        </div>
+      </Navbar>
     )
 }
