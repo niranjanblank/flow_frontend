9d5ffd35b1f4f95c9a65bfa19b98bc1b2f5c4682
Niranjan Shah <niranjanshah474@gmail.com>
2024-02-06 19:32:13 +1100

    form validation for creating board added using formik

--- app/components/Forms/CreateBoard.tsx
+++ app/components/Forms/CreateBoard.tsx
@@ -1,16 +1,40 @@
 "use client"
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
 export default function CreateBoard(){
+
+    const formik = useFormik({
+        initialValues: {
+            title: ''
+        },
+        validationSchema: Yup.object({
+            title: Yup.string()
+            .required("Required")
+            .min(4, "Must be at least 4 Characters")
+        }),
+        onSubmit: values => {
+            // Handle form submission here
+            console.log(values);
+        }
+    })
+
     return (
-        <div className="flex flex-col justify-center items-center bg-gray-600 text-white rounded-md w-64 p-4 gap-2">
+        <form 
+        onSubmit={formik.handleSubmit}
+        className="flex flex-col justify-center items-center bg-gray-600 text-white rounded-md w-64 p-4 gap-2">
                 <h1> Create board</h1>
                 <div className="bg-red mt-2">
                     <label className="text-xs"> Board title * </label>
-                    <input type="text" id="board_title"
+                    <input type="text" id="title"
                     className="w-full h-6 rounded-sm text-white bg-gray-800 px-2"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     ></input>
+                    {(formik.touched.title && formik.errors.title) && <p className="text-red-600 pt-2 text-xs">{formik.errors.title}</p>}
                 </div>
-                <button className="w-full h-6 bg-green-400 rounded hover:bg-green-600 ">Create</button>
-        </div>
+                <button type="submit" className="w-full h-6 bg-green-400 rounded hover:bg-green-600 ">Create</button>
+        </form>
     )
 }
