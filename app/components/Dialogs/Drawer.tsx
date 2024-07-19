79073d726638e9220ce2fd42cfe7493d211d978d
Niranjan Shah <niranjanshah474@gmail.com>
2024-07-19 22:26:32 +1000

    overlay and drawer animation separated

--- app/components/Dialogs/Drawer.tsx
+++ app/components/Dialogs/Drawer.tsx
@@ -6,8 +6,7 @@
 import { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 
-
-const Drawer = ({children, drawerComponent}:{children: React.ReactNode, drawerComponent: React.ReactNode}) => {
+const Drawer = ({ children, drawerComponent }: { children: React.ReactNode, drawerComponent: React.ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -18,30 +17,28 @@ const Drawer = ({children, drawerComponent}:{children: React.ReactNode, drawerCo
       >
         {drawerComponent}
       </button>
+      {/* Overlay */}
       <div
-        className={`fixed inset-0  z-50 transition-transform transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
+        className={`fixed inset-0 z-40 bg-black opacity-50 ${isOpen ? 'block' : 'hidden'}`}
+        onClick={() => setIsOpen(false)}
+      ></div>
+
+      {/* Drawer */}
+      <div
+        className={`fixed right-0 top-0 w-64 h-full bg-zinc-800 text-gray-400 shadow-lg p-4 z-50 transition-transform transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
       >
-        {/* background overlay */}
-        <div className="absolute inset-0 bg-black opacity-50" onClick={() => setIsOpen(false)}></div>
-        {/*  main drawer */}
-        <div className="absolute right-0 top-0 w-64 h-full bg-zinc-800 text-gray-400 shadow-lg p-4">
-          <div className='flex justify-between pb-2 items-center'>
-            <p className="font-bold">Menu</p>
-            <button
-              className="hover:bg-gray-500 p-2 text-xl hover:bg-opacity-10 hover:rounded-lg "
-              onClick={() => setIsOpen(false)}
-            >
-              <IoMdClose/>
-            </button>
-          </div>
-          
-          <hr className='border-gray-400'></hr>
-          <div className=" pt-2">
-          {/* <button onClick={()=>deleteBoard(id)}>Delete</button>
-           
-           */}
-           {children}
-          </div>
+        <div className='flex justify-between pb-2 items-center'>
+          <p className="font-bold">Menu</p>
+          <button
+            className="hover:bg-gray-500 p-2 text-xl hover:bg-opacity-10 hover:rounded-lg"
+            onClick={() => setIsOpen(false)}
+          >
+            <IoMdClose/>
+          </button>
+        </div>
+        <hr className='border-gray-400'></hr>
+        <div className="pt-2">
+          {children}
         </div>
       </div>
     </>
