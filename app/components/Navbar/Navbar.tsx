e5a39f618dcad9f3165f87cfb667dc7357020a07
Niranjan Shah <niranjanshah474@gmail.com>
2024-07-22 22:27:25 +1000

    header with a link to boards added to Navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import Link from "next/link"
 import { useState } from "react"
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa"
+
 export function Navbar({
     children
 }: {children: React.ReactNode}){
@@ -13,6 +15,16 @@ export function Navbar({
     }
     return (
         <div className={`bg-zinc-800 text-gray-200 ${open?'min-w-72':'min-w-8'} flex flex-col transition-all duration-100 p-2 gap-2`}>
+               {/* Header */}
+               {
+                open?( <Link className="flex justify-center p-10 items-center text-white" href="/boards">
+                    <h1 className="text-3xl font-bold">SoloPlanner</h1>
+                </Link>):(<Link className="flex justify-center py-10 items-center text-white" href="/boards">
+                    <h1 className="text-3xl font-bold">S</h1>
+                </Link>)
+               }
+              
+            <hr/>
             <div className="flex justify-between ">
                 {open?(<h1>Username Here</h1>):""}
                 <button className="text-lg" onClick={onCloseHandler}>{open?<FaAngleLeft/>:<FaAngleRight/>}</button>
