603000d00d2ef2fd50ddb450340b592c301cc4dc
Niranjan Shah <niranjanshah474@gmail.com>
2024-08-23 18:30:43 +1000

    center layout of login form completed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/Forms/LoginForm";
 
 export default function Login(){
     return (
-        <div className="w-screen h-screen flex justify-center items-center -mt-40">
+        <div className="w-screen h-screen flex justify-center items-center">
         <LoginForm/>
         </div>
     )
